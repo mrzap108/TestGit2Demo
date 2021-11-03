5c15c60d05fe1e17871b6e7813000560b4773f2f
zante palermo <mrzap108@gmail.com>
2021-11-03 11:03:39 +0800

    Main Program

---
+++ TestCodesInGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCodesInGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
